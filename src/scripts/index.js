a0613124a93ad8b8c5c71fed2c1dac26edae5366
Nada Hasni Muhammad <nadasthing@gmail.com>
2021-01-05 19:18:54 +0700

    revised-by-mas-lukman-dicoding

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,8 +3,6 @@ import "../styles/main.css";
 import "../styles/responsive.css";
 import data from "../DATA.json";
 
-console.log("Hello Coders! :)");
-
 const menu = document.querySelector("#menu");
 const hero = document.querySelector(".hero");
 const main = document.querySelector("main");
@@ -14,6 +12,46 @@ const posts = document.querySelector("#posts");
 //data restaurant
 const listRestaurant = data.restaurants;
 
+//function
+function createSummary(text, length) {
+  if (text.length > length) {
+    text = text.slice(0, length);
+    text += " ...";
+    return text;
+  } else {
+    return text;
+  }
+}
+
+function renderRestaurants(list) {
+  list.forEach((data) => {
+    var description = createSummary(data.description, 200);
+
+    posts.innerHTML += `
+      <article class="post-item">
+      <img
+        class="post-item__thumbnail"
+        src="${data.pictureId}"
+        alt="${data.name}"
+      />
+      <div class="post-item__city">
+        <p>Kota ${data.city}</p>
+      </div>
+      <div class="post-item__content">
+        <p class="post-item__rating">Rating:</p>
+        <p class="post-item__rating_value">${data.rating}</p>
+        <h1 class="post-item__title">
+          <a href="#">${data.name}</a>
+        </h1>
+        <p class="post-item__description">
+          ${description}
+        </p>
+      </div>
+    </article>           
+      `;
+  });
+}
+
 //menu hamburger event listener
 menu.addEventListener("click", function (event) {
   drawer.classList.toggle("open");
@@ -21,7 +59,7 @@ menu.addEventListener("click", function (event) {
 });
 
 menu.addEventListener("keypress", function (event) {
-  if (event.key === "Enter" || event.keyCode === 32) {
+  if (event.key === "Enter") {
     event.preventDefault();
     event.stopPropagation();
     drawer.classList.toggle("open");
@@ -37,33 +75,4 @@ main.addEventListener("click", function () {
 });
 
 // render list restaurant article
-listRestaurant.forEach((data) => {
-  var description = data.description;
-  if (description.length > 200) {
-    description = description.slice(0, 200);
-    description += " ...";
-  }
-
-  posts.innerHTML += `
-    <article class="post-item">
-    <img
-      class="post-item__thumbnail"
-      src="${data.pictureId}"
-      alt="${data.name}"
-    />
-    <div class="post-item__city">
-      <p>Kota ${data.city}</p>
-    </div>
-    <div class="post-item__content">
-      <p class="post-item__rating">Rating:</p>
-      <p class="post-item__rating_value">${data.rating}</p>
-      <h1 class="post-item__title">
-        <a href="#">${data.name}</a>
-      </h1>
-      <p class="post-item__description">
-        ${description}
-      </p>
-    </div>
-  </article>           
-    `;
-});
+renderRestaurants(listRestaurant);
